9f79064e98fc9aa3d6c2e3aac559279a71ee2d00
Murat Sahin <yazilimogren1111@gmail.com>
2020-11-21 22:29:30 +0100

    created project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hahay</Text>
+    </View>
+  )
+}
+export default App
